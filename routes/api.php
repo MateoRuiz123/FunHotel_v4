7caac8e45a3d6092db160cf95eaeeb27dc8cf3bf
Bri <briyiderazo@gmail.com>
2023-07-26 11:15:37 -0500

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\userApiController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ServicioController;
@@ -27,41 +28,7 @@ use App\Http\Controllers\PagoController;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('/clientes', ClienteController::class);
-    Route::apiResource('/servicios', ServicioController::class);
-    Route::apiResource('/catalogos', CatalogoController::class);
-    Route::apiResource('/ventas', VentaControlador::class);
-    Route::apiResource('/categorias', CategoriaController::class);
-    Route::apiResource('/habitaciones', HabitacionController::class);
-    Route::apiResource('/roles', RoleController::class);
-    Route::apiResource('/users', UserController::class);
-    Route::apiResource('/reservas', ReservaController::class);
-    Route::apiResource('/products', ProductController::class);
-    Route::apiResource('/groups', GroupController::class);
-    Route::apiResource('/checkins', CheckinController::class);
-    Route::apiResource('/checkouts', CheckoutController::class);
-    Route::apiResource('/pagos', PagoController::class);
-
-    Route::get('/ventas-obtener-informacion-reserva/{id}', [VentaControlador::class, 'obtenerInformacionReserva']);
-    Route::get('/venta-create', [VentaControlador::class, 'create'])->name('ventas.create');
-    Route::get('/venta-update/{venta}', [VentaControlador::class, 'edit'])->name('ventas.edit');
-
-    // Ruta para añadir usuarios a un grupo
-    Route::post('/groups/{group}/add-user', [GroupController::class, 'addUser'])->name('groups.addUser');
-    // Ruta para eliminar usuarios de un grupo
-    Route::delete('/groups/{group}/users/{user}', [GroupController::class, 'removeUser'])->name('groups.users.remove');
-
-    Route::get('users/assign-roles', [UserController::class, 'show'])->name('users.assign-roles');
-    Route::post('users/save-roles', [UserController::class, 'saveRoles'])->name('users.save-roles');
-
-    Route::post('/groups/{group}/assign-roles', [GroupController::class, 'assignRoles'])->name('groups.assignRoles');
-    Route::post('/groups/{group}/users/{user}/assign-role', [GroupController::class, 'assignUserRole'])->name('groups.assignUserRole');
-    Route::post('/groups/{group}/assign-group-role', [GroupController::class, 'assignGroupRole'])->name('groups.assignGroupRole');
-    Route::post('/groups/{group}/revoke-group-role', [GroupController::class, 'revokeGroupRole'])->name('groups.revokeGroupRole');
+Route::prefix('api')->group(function () {
+    Route::get('/users', [userApiController::class, 'index']);
 });
 
-
-use App\Http\Controllers\API\AuthController;
-
-Route::post('/login', [AuthController::class, 'login']);
