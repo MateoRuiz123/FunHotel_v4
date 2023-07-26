a43f6c832219ef76b64d66b1d0b63cf02a6743be
Mateo <mrivera736@soy.sena.edu.co>
2023-07-26 07:23:58 -0500

    intento de api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ServicioController;
+use App\Http\Controllers\CatalogoController;
+use App\Http\Controllers\VentaControlador;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\HabitacionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\CheckinController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\PagoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +22,46 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider and will be assigned to the
+| "api" middleware group. Start building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/clientes', ClienteController::class);
+    Route::apiResource('/servicios', ServicioController::class);
+    Route::apiResource('/catalogos', CatalogoController::class);
+    Route::apiResource('/ventas', VentaControlador::class);
+    Route::apiResource('/categorias', CategoriaController::class);
+    Route::apiResource('/habitaciones', HabitacionController::class);
+    Route::apiResource('/roles', RoleController::class);
+    Route::apiResource('/users', UserController::class);
+    Route::apiResource('/reservas', ReservaController::class);
+    Route::apiResource('/products', ProductController::class);
+    Route::apiResource('/groups', GroupController::class);
+    Route::apiResource('/checkins', CheckinController::class);
+    Route::apiResource('/checkouts', CheckoutController::class);
+    Route::apiResource('/pagos', PagoController::class);
+
+    Route::get('/ventas-obtener-informacion-reserva/{id}', [VentaControlador::class, 'obtenerInformacionReserva']);
+    Route::get('/venta-create', [VentaControlador::class, 'create'])->name('ventas.create');
+    Route::get('/venta-update/{venta}', [VentaControlador::class, 'edit'])->name('ventas.edit');
+
+    // Ruta para añadir usuarios a un grupo
+    Route::post('/groups/{group}/add-user', [GroupController::class, 'addUser'])->name('groups.addUser');
+    // Ruta para eliminar usuarios de un grupo
+    Route::delete('/groups/{group}/users/{user}', [GroupController::class, 'removeUser'])->name('groups.users.remove');
+
+    Route::get('users/assign-roles', [UserController::class, 'show'])->name('users.assign-roles');
+    Route::post('users/save-roles', [UserController::class, 'saveRoles'])->name('users.save-roles');
+
+    Route::post('/groups/{group}/assign-roles', [GroupController::class, 'assignRoles'])->name('groups.assignRoles');
+    Route::post('/groups/{group}/users/{user}/assign-role', [GroupController::class, 'assignUserRole'])->name('groups.assignUserRole');
+    Route::post('/groups/{group}/assign-group-role', [GroupController::class, 'assignGroupRole'])->name('groups.assignGroupRole');
+    Route::post('/groups/{group}/revoke-group-role', [GroupController::class, 'revokeGroupRole'])->name('groups.revokeGroupRole');
 });
+
+
+use App\Http\Controllers\API\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c3504cf8153230d7f00ef638c4c8717ccb48d249',
+        'reference' => 'a431da3ecb773b2dfcb95e943c9785ddd1e7046c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -373,7 +373,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c3504cf8153230d7f00ef638c4c8717ccb48d249',
+            'reference' => 'a431da3ecb773b2dfcb95e943c9785ddd1e7046c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -733,8 +733,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
