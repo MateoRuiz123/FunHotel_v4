f1ef1bb255991c574f9ef8715e4eb4817441bdf0
Jeidy <jeidy@gmail.com>
2023-08-01 18:07:25 -0500

    Lnading Loggin cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -16,14 +15,14 @@
             font-size: 16px;
             background-color: #fff;
             border: 1px solid #ccc;
-            border-radius: 4px;
+            border-radius: 8px;
             cursor: pointer;
             transition: background-color 0.3s, color 0.3s;
         }
 
         .back-button:hover {
-            background-color: #614bf1;
-            color: #fff;
+            background-color: #614bf1e1;
+            color: #ffffff;
         }
     </style>
     <script>
@@ -44,7 +43,6 @@
         }
     </script>
 </head>
-
 <body>
     
     <main>
@@ -58,9 +56,7 @@
                     <form action="{{ route('login') }}" autocomplete="off" class="sign-in-form" method="POST">
                         @csrf
                         <div class="heading"><br><br>
-                            <h2>{{ __('Bienvenidos') }}</h2>
-                            <h4>{{ __('¿Quiere registrarse?') }}</h4>
-                            <a href="#" class="toggle">{{ __('Registrarse') }}</a><br><br>
+                            <h2>{{ __('Bienvenidos') }}</h2><br><br>
                         </div><br>
                         <div class="actual-form">
                             <div class="input-wrap">
@@ -93,97 +89,7 @@
                             </p>
                         </div>
                     </form>
-
-                    <form action="{{ route('register') }}" autocomplete="off" class="sign-up-form" method="POST">
-                        @csrf
-                        <div class="inner-box-registrar">
-                            <div class="campo-contenedor">
-                                <div class="heading">
-                                    <h2>Registro</h2>
-                                    <h4>¿Ya tienes una cuenta?</h4>
-                                    <a href="#" class="toggle">Ingresar</a>
-                                </div><br>
-                                <div class="input-w">
-                                    <input id="name" name="name" autofocus type="text"
-                                        class="input-f @error('name') is-invalid @enderror" autocomplete="off"
-                                        required />
-                                    <label>{{ __('Nombre') }}</label>
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="input-w">
-                                    <input id="second_name" name="second_name" type="text" class="input-f"
-                                        autocomplete="off" />
-                                    <label>{{ __('Segundo nombre') }}</label>
-                                </div>
-
-                                <div class="input-w">
-                                    <input id="surname" name="surname" autofocus type="text"
-                                        class="input-f @error('surname') is-invalid @enderror" autocomplete="off"
-                                        required />
-                                    <label>{{ __('Apellido') }}</label>
-                                    @error('surname')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="input-w">
-                                    <input id="second_surname" name="second_surname" type="text" class="input-f" />
-                                    <label>{{ __('Segundo apellido') }}</label>
-                                </div>
-                                <div class="input-w">
-                                    <input id="birthday" name="birthday" autofocus type="date"
-                                        class="input-f @error('birthday') is-invalid @enderror" autocomplete="off"
-                                        required />
-                                    <label>{{ __('Fecha de nacimiento') }}</label>
-                                    @error('birthday')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="input-w">
-                                    <input id="email" name="email" autofocus type="email"
-                                        class="input-f @error('email') is-invalid @enderror" autocomplete="off"
-                                        required />
-                                    <label>{{ __('Correo Electronico') }}</label>
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="input-w">
-                                    <div id="ver" class="campo1" onclick="showHide('password1', 'campo1');">
-                                    </div>
-                                    <input id="password1" name="password" type="password" minlength="4"
-                                        class="input-f @error('password') is-invalid @enderror" autocomplete="off"
-                                        required onpaste="return false" />
-                                    <label>{{ __('Contraseña') }}</label>
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="input-w">
-                                    <div id="ver" class="campo2"
-                                        onclick="showHide('password-confirm', 'campo2');"></div>
-                                    <input id="password-confirm" name="password_confirmation" type="password"
-                                        minlength="4" class="input-f" autocomplete="off" required
-                                        onpaste="return false" />
-                                    <label>{{ __('Confirmar contraseña') }}</label><br>
-                                </div>
-                            </div>
-                            <input type="submit" value="Registrarse" class="sign-btn" />
-                        </div>
-                </div>
-                </form>
-            </div>
+            </div> </div>
             <div class="carousel">
                 <div class="images-wrapper">
                     <img src="{{ asset('img/Hotel.png') }}" class="image img-1 show" alt="" />
@@ -210,7 +116,6 @@
     </main>
     <script src="{{ asset('estilo.js') }}"></script>
 </body>
-
 </html>
 
 {{-- En el código proporcionado, he agregado el atributo `onpaste="return false"` a los campos de contraseña en ambos formularios (inicio de sesión y registro). Esto evitará que los usuarios puedan pegar contenido en esos campos. Sin embargo, es importante tener en cuenta que esto no proporciona una seguridad completa, ya que los usuarios aún pueden ingresar manualmente los datos. Es recomendable implementar medidas adicionales, como validación en el lado del servidor y el uso de técnicas como cifrado de contraseñas para garantizar una mayor seguridad en el manejo de contraseñas. --}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,40 +8,42 @@
     <link rel="stylesheet" href="{{asset('css/land.css')}}">
     <script src="{{asset('js/land.js')}}"></script>
 </head>
-
 <body>
     <header>
-        <nav>
-            <ul class="nav nav-tabs">
-                <li><a href="/home">Iniciar sesión</a></li>
-                <li><a href="/habitaciones">Habitaciones</a></li>
-                <li><a href="#Promociones">Promociones</a></li>
-                <li><a href="/servicios">Servicios</a></li>
-                <li><a href="#contacto">Contacto</a></li>
-            </ul>
-
-        </nav>
+    <nav>
+        <ul class="nav nav-tabs">
+            <li><a href="/home">Iniciar sesión</a></li>
+            <li><a href="/habitaciones">Habitaciones</a></li>
+            <li><a href="#Promociones">Promociones</a></li>
+            <li><a href="/servicios">Servicios</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </nav>
     </header>
     <section id="inicio">
-        <div class="hero"><br><br><br>
-            <h1 class="animate-letters">Bienvenidos a <br>FunHotel</h1>
-            <a href="/reservas" class="btn">Reserva ahora</a>
-        </div>
+      <div class="hero">
+        <h1 class="animate-letters">Bienvenidos a <br>FunHotel</h1>
+        <a href="#reservas" class="btn">Reserva ahora</a>
+      </div>
     </section>
-    <br><br><br><br><br>
-    <center><br><br><br><br>
-        <section id="mision-vision">
-            <div class="container">
-                <h2>Misión</h2>
-                <div class="mission">
-                    <p>Fun Hotel es un aplicativo web API en donde los estudiantes obtendrán el rol de administrador de
-                        un hotel
-                        y podrán gestionar sus servicios que, a su vez, tendrá niveles de dificultad por temporadas.
-                    </p>
-                </div>
+    <div class="container">
+        <div class="nuevo">
+          <h2 class="animate-letters">Misión</h2>
+            <p>En FunHotel, nuestra misión es brindar a nuestros clientes una experiencia <br> inigualable de comodidad, lujo y diversión. Nos esforzamos por ser <br> un referente en la industria hotelera, ofreciendo un 
+              servicio <br> excepcional y superando las expectativas de nuestros huéspedes. <br>
+              Nuestro objetivo principal es garantizar que cada momento que nuestros <br> clientes pasen con nosotros sea memorable.
+            </p>
+          </div>
+
+          <div class="nuevo">
+            <h2 class="animate-letters">Visión</h2>
+              <p>Nos esforzamos por ser un referente en la industria hotelera, destacándonos por nuestra excelencia 
+                en la calidad y la innovación. Buscamos constantemente mejorar nuestras  
+                instalaciones y servicios para ofrecer experiencias únicas y sobresalientes.
+              </p>
             </div>
-        </section>
-        <ul class="cards">
+          </div><br><br><br><br>
+          <ul class="cards">
             <li class="cards__item">
                 <div class="card">
                     <div class="card__image card__image--Andrw"></div>
@@ -64,7 +66,7 @@
             </li>
             <li class="cards__item">
                 <div class="card">
-                    <div class="card__image card__image--Jeidy"></div>
+                    <div class="card__image card__image--Mateo"></div>
                     <div class="card__content">
                         <div class="card__title">Mateo Ruiz</div>
                         <p class="card__text">Programador/Diseñador</p>
@@ -92,23 +94,20 @@
                     </div>
                 </div>
             </li>
-        </ul>
-    </center>
+          </ul>
 
-    <section id="contacto">
+      <section id="contacto">
         <h2>Contacto</h2>
-        <p>¡Estaremos encantados de ayudarte! Puedes contactarnos a través de los siguientes medios:</p>
+        <p>¡Estaremos encantados de ayudarte! <br> Puedes contactarnos a través de los siguientes medios:</p>
         <ul>
-            <li>Teléfono: 123-456-789</li>
-            <li>Email: info@funhotel.com</li>
-            <li>Dirección: Calle Principal, Medellín</li>
+          <li>Teléfono: 314-885-23-54</li>
+          <li>Email: info@funhotel.com</li>
+          <li>Dirección: Calle Principal, Medellín</li>
         </ul>
-    </section>
+      </section>
+
     <footer>
-        <p>&copy; 2023 FunHotel. Todos los derechos reservados.</p>
+      <p>&copy; 2023 FunHotel. Todos los derechos reservados.</p>
     </footer>
-
-
 </body>
-
 </html>
