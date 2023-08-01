6f75593d5bee806d5766886556099ef4c85a14a6
MateoRuiz123 <mrivera736@soy.sena.edu.co>
2023-08-01 07:24:07 -0500

    laravel lang

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -21,6 +21,11 @@
             </div>
         </div>
     </div>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
     <div class="col-12">
         <div class="card">
             <div class="card-body">
