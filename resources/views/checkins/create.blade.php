bed5e46897532050f17a7e3d246f9f7116a6060c
MateoRuiz123 <mrivera736@soy.sena.edu.co>
2023-08-29 10:48:22 -0500

    venta

--- resources/views/checkins/create.blade.php
+++ resources/views/checkins/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,8 +12,8 @@
         function checkConfirmacion() {
             var ingreso = $('#ingreso').val().trim();
             var reserva = $('#reserva').val().trim();
-    
-            if (ingreso === '' || reserva === '' ) {
+
+            if (ingreso === '' || reserva === '') {
                 Swal.fire({
                     title: 'Campos vacíos',
                     text: 'Por favor, completa todos los campos antes de continuar.',
@@ -38,46 +39,49 @@
         }
     </script>
 </head>
+
 <body>
     <div class="modal fade" id="create" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="modalCreateLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Agregar check-in</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                </button>
-            </div>
-            <div class="modal-body">
-                <form id="Cheform" class="row g-3" action="{{ route('checkins.store') }}" method="post"
-                    enctype="multipart/form-data">
-                    @csrf
-                    <div class="col-md-6">
-                        <label for="" class="form-label"> Fecha de ingreso</label>
-                        <input type="date" class="form-control" name="ingreso" id="ingreso"
-                            aria-describedby="helpId" placeholder="">
-                    </div>
+        aria-labelledby="modalCreateLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Agregar check-in</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="Cheform" class="row g-3" action="{{ route('checkins.store') }}" method="post"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="col-md-6">
+                            <label for="" class="form-label"> Fecha de ingreso</label>
+                            <input type="date" class="form-control" name="ingreso" id="ingreso"
+                                aria-describedby="helpId" placeholder="">
+                        </div>
 
-                    <div class="col-md-6">
-                        <label for="" class="form-label">Id Reserva</label>
-                        <select class="form-select" name="reserva" id="reserva" required>
-                            <option value="">Seleccione</option>
-                            @foreach ($reservas as $reserva)
-                                <option value="{{ $reserva->id }}">{{ $reserva->id }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div>
-                        <input type="hidden" name="estado" id="estado" value="{{ app\models\Checkin::Activo }}">
-                    </div>
-            </div>
-        </form>
-            <div class="modal-footer">
-                <button type="submit" onclick="checkConfirmacion()" class="btn btn-primary">Crear</button>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <div class="col-md-6">
+                            <label for="" class="form-label">Id Reserva</label>
+                            <select class="form-select" name="reserva" id="reserva" required>
+                                <option value="">Seleccione</option>
+                                @foreach ($reservas as $reserva)
+                                    <option value="{{ $reserva->id }}">{{ $reserva->id }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <input type="hidden" name="estado" id="estado"
+                                value="{{ app\models\Checkin::Activo }}">
+                        </div>
+                </div>
+                </form>
+                <div class="modal-footer">
+                    <button type="submit" onclick="checkConfirmacion()" class="btn btn-primary">Crear</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
